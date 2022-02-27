18d3609adf4d14872800771f2101ea0260ca9cfa
Alexey Kulish <alexkulish1993@gmail.com>
2022-02-27 15:51:20 +0300

    Initial Commit

---
+++ GuessNumberApp/GuessNumberAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberAppApp.swift
+//  GuessNumberApp
+//
+//  Created by Alex Kulish on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
